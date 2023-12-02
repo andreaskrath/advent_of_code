4535bc457707dc5782a78f6a74812eef108d0a7d
Andreas Krath <andreas.krath+github@gmail.com>
2023-12-02 11:30:14 +0100

    2023: solve day 2 part 1 and part 2

---
+++ 2023/day2/src/main.rs
@@ -0,0 +1,115 @@
+use std::time::Instant;
+
+fn main() {
+    let input = include_str!("../input.txt");
+
+    let part1_start = Instant::now();
+    let part1_res = part1(input);
+    let part1_stop = part1_start.elapsed();
+    println!("Part 1 is '{}' in {:2.?}", part1_res, part1_stop);
+
+    let part2_start = Instant::now();
+    let part2_res = part2(input);
+    let part2_stop = part2_start.elapsed();
+    println!("Part 2 is '{}' in {:2.?}", part2_res, part2_stop);
+}
+
+fn part1(input: &str) -> usize {
+    let mut game_id = 1;
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let mut valid_game = true;
+        let pattern = format!("Game {}:", game_id);
+        let stripped = line.strip_prefix(pattern.as_str()).unwrap();
+        let sets = stripped.split(';');
+
+        'target: for set in sets {
+            let cubes = set.split(',');
+            for cube in cubes {
+                let (num, color) = cube.trim().split_once(' ').unwrap();
+                let num: usize = num.parse().unwrap();
+                match color {
+                    "red" => {
+                        if num > 12 {
+                            valid_game = false;
+                            break 'target;
+                        }
+                    }
+                    "green" => {
+                        if num > 13 {
+                            valid_game = false;
+                            break 'target;
+                        }
+                    }
+                    "blue" => {
+                        if num > 14 {
+                            valid_game = false;
+                            break 'target;
+                        }
+                    }
+                    _ => unreachable!("colors are bounded and this arm should not be possible"),
+                }
+            }
+        }
+
+        if valid_game {
+            sum += game_id;
+        }
+        game_id += 1;
+    }
+
+    sum
+}
+
+fn part2(input: &str) -> usize {
+    let mut game_id = 1;
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let pattern = format!("Game {}:", game_id);
+        let stripped = line.strip_prefix(pattern.as_str()).unwrap();
+        let sets = stripped.split(';');
+
+        let (mut red, mut green, mut blue) = (0, 0, 0);
+        for set in sets {
+            let cubes = set.split(',');
+            for cube in cubes {
+                let (num, color) = cube.trim().split_once(' ').unwrap();
+                let num: usize = num.parse().unwrap();
+                match color {
+                    "red" => red = red.max(num),
+                    "green" => green = green.max(num),
+                    "blue" => blue = blue.max(num),
+                    _ => unreachable!("colors are bounded and this arm should not be possible"),
+                }
+            }
+        }
+
+        sum += red * green * blue;
+        game_id += 1;
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+
+    #[test]
+    fn part1_sample() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let expected = 8;
+        let actual = part1(input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn part2_sample() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let expected = 2286;
+        let actual = part2(input);
+        assert_eq!(actual, expected);
+    }
+}
