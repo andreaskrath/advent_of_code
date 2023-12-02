410216fe5b00c0c86469ac19834ccc1f7c7709a1
Andreas Krath <andreas.krath+github@gmail.com>
2023-12-02 01:19:40 +0100

    2023: solve day 1 part 2
    struggled somewhat with overlapping words being a thing but not represented properly in the sample showcase, but figured it out eventually

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,5 +1,16 @@
 use std::time::Instant;
 
+const NUMBER_WORDS: [(&str, char); 9] = [
+    ("one", '1'),
+    ("two", '2'),
+    ("three", '3'),
+    ("four", '4'),
+    ("five", '5'),
+    ("six", '6'),
+    ("seven", '7'),
+    ("eight", '8'),
+    ("nine", '9'),
+];
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -8,6 +19,11 @@ fn main() {
     let part1_res = part1(input);
     let part1_stop = part1_start.elapsed();
     println!("Part 1 is '{}' in {:2.?}", part1_res, part1_stop);
+
+    let part2_start = Instant::now();
+    let part2_res = part2(input);
+    let part2_stop = part2_start.elapsed();
+    println!("Part 2 is '{}' in {:2.?}", part2_res, part2_stop);
 }
 
 fn part1(input: &str) -> u32 {
@@ -24,6 +40,38 @@ fn part1(input: &str) -> u32 {
         .sum()
 }
 
+fn part2(input: &str) -> u32 {
+    let replaced = input
+        .lines()
+        .map(|s| {
+            let s = s.as_bytes();
+            let mut transformed = String::new();
+            let mut i = 0;
+            while i < s.len() {
+                if (s[i] as char).is_ascii_digit() {
+                    transformed.push(s[i] as char);
+                    i += 1;
+                    continue;
+                }
+
+                for (word, digit) in NUMBER_WORDS {
+                    if s[i..].starts_with(word.as_bytes()) {
+                        transformed.push(digit);
+                        i += 1;
+                        continue;
+                    }
+                }
+
+                i += 1;
+            }
+
+            transformed
+        })
+        .collect::<Vec<String>>();
+
+    part1(replaced.join("\n").as_str())
+}
+
 #[cfg(test)]
 mod part1 {
     use crate::part1;
@@ -37,3 +85,15 @@ mod part1 {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::part2;
+
+    #[test]
+    fn sample() {
+        let input = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen";
+        let expected = 281;
+        let actual = part2(input);
+        assert_eq!(actual, expected);
+    }
+}
